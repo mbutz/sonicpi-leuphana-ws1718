c8ecc74f677a5dbcd79541d9ba7d6ac011128cab
G. Martin Butz <mb@mkblog.org>
2017-11-14 10:13:11 +0100

    Added drum beat homework.

---
+++ session-03/hausaufgabe_Anngret_DrumBeat_12-11-17.rb
@@ -0,0 +1,27 @@
+#Drum Beat 12.11.17
+
+use_bpm 120
+
+4.times do
+  sample :bd_808
+  sleep 1
+end
+
+live_loop :basedrum do
+  sample :bd_808
+  sleep 1
+end
+
+
+live_loop :cymbal do
+  rhythm = (ring 0,1,0,1)
+  if rhythm.tick == 1
+    sample :drum_cymbal_closed, amp: 0.05
+  end
+  sleep 0.5
+end
+
+live_loop :akzent do
+  sample :drum_roll, amp: 0.05, sustain: 2, release: 0.5
+  sleep 8
+end
